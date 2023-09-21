2e084b9c57ef674c8440ac37de1f477ab72d62fa
Eazyisreal <eazyisreal192@gmail.com>
2023-09-21 06:59:31 +0100

    [Feat]: Drag and drop feature built

--- image-gallery/src/components/ImageGrid.jsx
+++ image-gallery/src/components/ImageGrid.jsx
@@ -1,12 +1,44 @@
 import imageData from '../data.json';
+import { useState } from 'react';
 
 export default function ImageGrid() {
+  const [images, setImages] = useState(imageData);
+  const [draggedImage, setDraggedImage] = useState(null);
+
+  const handleDragStart = (e, image) => {
+    setDraggedImage(image);
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const handleDrop = (e, targetImage) => {
+    e.preventDefault();
+
+    const updatedImages = [...images];
+
+    const draggedIndex = updatedImages.findIndex((img) => img.id === draggedImage.id);
+    const targetIndex = updatedImages.findIndex((img) => img.id === targetImage.id);
+
+    [updatedImages[draggedIndex], updatedImages[targetIndex]] = [updatedImages[targetIndex], updatedImages[draggedIndex]];
+
+    setImages(updatedImages);
+  };
+
   return (
     <section className="p-4 bg-gray-100">
       <div className="container mx-auto">
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
-          {imageData.map((image) => (
-            <div key={image.id} className="p-2">
+          {images.map((image) => (
+            <div
+              key={image.id}
+              className="p-2"
+              draggable
+              onDragStart={(e) => handleDragStart(e, image)}
+              onDragOver={(e) => handleDragOver(e)}
+              onDrop={(e) => handleDrop(e, image)}
+            >
               <div className="bg-white rounded-lg shadow-md">
                 <img
                   src={image.src}
