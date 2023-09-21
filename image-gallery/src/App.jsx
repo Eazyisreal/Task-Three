97a528079618bf0538e4a0da10296031df57dd1c
Eazyisreal <eazyisreal192@gmail.com>
2023-09-21 06:50:52 +0100

    [Feat]: User Auth created using Clerk

--- image-gallery/src/App.jsx
+++ image-gallery/src/App.jsx
@@ -1,11 +1,26 @@
-import './App.css';
+import "./App.css";
 
-import ImageGrid from './components/ImageGrid';
+import {
+  ClerkProvider,
+  RedirectToSignIn,
+  SignedIn,
+  SignedOut,
+} from "@clerk/clerk-react";
+
+import ImageGrid from "./components/ImageGrid";
+
+const clerkPublishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
 
 export default function App() {
   return (
-    <>
+    <ClerkProvider publishableKey={clerkPublishableKey}>
+    <SignedIn>
       <ImageGrid />
-    </>
+    </SignedIn>
+    <SignedOut>
+      <RedirectToSignIn />
+    </SignedOut>
+  </ClerkProvider>
   );
 }
--- image-gallery/src/components/ImageGrid.jsx
+++ image-gallery/src/components/ImageGrid.jsx
@@ -1,29 +1,31 @@
 import imageData from '../data.json';
 
-export default function ImageGrid () {
+export default function ImageGrid() {
   return (
-    <section>
-      <div className="container">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-2">
+    <section className="p-4 bg-gray-100">
+      <div className="container mx-auto">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
           {imageData.map((image) => (
-            <div
-              key={image.id}
-              className={`${
-                image.category === 'Nature'
-                  ? 'col-span-2'
-                  : 'col-span-1'
-              }`}
-            >
-              <img
-                src={image.src}
-                alt={image.alt}
-                className="w-full h-auto"
-              />
+            <div key={image.id} className="p-2">
+              <div className="bg-white rounded-lg shadow-md">
+                <img
+                  src={image.src}
+                  alt={image.alt}
+                  className="w-full h-auto rounded-t-lg"
+                />
+                <div className="p-4">
+                  <p className="text-sm text-gray-600">{image.description}</p>
+                  <div className="mt-2">
+                    <span className="inline-block px-2 py-1 mr-2 text-xs font-semibold text-white bg-blue-500 rounded-full">
+                      {image.category}
+                    </span>
+                  </div>
+                </div>
+              </div>
             </div>
           ))}
         </div>
       </div>
     </section>
   );
-};
-
+}
